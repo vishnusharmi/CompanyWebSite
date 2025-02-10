6ea510fc95896e9ccf3b2892cbf2beeaf1223186
Reshma0711 <reshmamudedla@gmail.com>
2025-02-10 17:56:45 +0530

    made a change

--- company-website/src/components/adminDash/layout/SiderBar.jsx
+++ company-website/src/components/adminDash/layout/SiderBar.jsx
@@ -9,7 +9,7 @@ import {
   FaSignOutAlt,
 } from "react-icons/fa";
 import { TbLayoutDashboardFilled } from "react-icons/tb";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 
 
