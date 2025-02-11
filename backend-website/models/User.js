306331b72144f1ca01c8f50c839abbcf6c73e8e0
rasagnauppula01 <rasagnauppula10@gmail.com>
2025-02-11 16:27:38 +0530

    clear-conflicts-in-backend-code

--- backend-website/models/User.js
+++ backend-website/models/User.js
@@ -1,8 +1,8 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
 
-const User = sequelize.define("User", {
-    user_id: {
+const Users = sequelize.define('users', {
+  id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
